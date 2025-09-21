8494b801cd2c1530018b0197575a09cc3bea0340
Home <ivan@ivancace.com>
2025-09-21 10:47:57 +0200

    Add more lsp settings

--- lua/ivan/plugins/lsp.lua
+++ lua/ivan/plugins/lsp.lua
@@ -184,6 +184,9 @@ return {
         intelephense = {},
         pyright = {},
         rust_analyzer = {},
+        csharp_ls = {},
+        clangd = {},
+        terraform = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
