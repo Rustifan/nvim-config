ac5e6471739bd0a609c1f335431f5942588e3b57
Ivan <ivan.cace@kompare.hr>
2025-09-22 09:13:32 +0200

    Add default indent for typescritpt

--- lua/ivan/plugins/indent.lua
+++ lua/ivan/plugins/indent.lua
@@ -1,1 +1,12 @@
-return { 'NMAC427/guess-indent.nvim' }
+return {
+  'NMAC427/guess-indent.nvim',
+  config = function()
+    vim.api.nvim_create_autocmd({ 'FileType' }, {
+      pattern = { 'typescript' },
+      callback = function()
+        vim.opt_local.shiftwidth = 2
+        vim.opt_local.tabstop = 2
+      end,
+    })
+  end,
+}
